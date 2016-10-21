0635e2a24161fdab8638858ebdbf2d8550fc0f88
Fraz Ahsan <fraz.ahsan17@gmail.com>
2016-10-21 17:46:30 +0500

    Default image added

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
 	validates_presence_of :name, :number
 
-	has_attached_file :logo, styles: { thumb: "100x100>" }
+	has_attached_file :logo, styles: { thumb: "100x100>" }, :default_url => "/assets/:style/missing.jpg"
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
 
   before_save :set_keywords
