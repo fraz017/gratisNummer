39b43c63c4fb9e22a3a547f58c37b001e9de91d1
Fraz Ahsan <fraz.ahsan17@gmail.com>
2016-10-23 17:38:32 +0500

    Production path changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,8 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  Paperclip.options.merge!(:command_path => "/usr/bin")
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
