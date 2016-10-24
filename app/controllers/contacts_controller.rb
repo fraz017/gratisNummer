404c2bb2bc64ff2f41e94bf051210f382ef46c58
Fraz Ahsan <fraz.ahsan17@gmail.com>
2016-10-24 16:37:20 +0500

    Auth removed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:show, :all]
+  before_action :authenticate_user!, except: [:show, :all_contacts]
 
   # GET /contacts
   # GET /contacts.json
